e125d02bf4f14acd2fc9cb4e0e1e8a943135b182
Ester Legaz <esterlegaz@gmail.com>
2019-01-24 22:53:14 +0100

    Upload docs

---
+++ docs/precache-manifest.4a8f1ea66cd562b5be167085e20857a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5540092c179caa2267d7",
+    "url": "./static/js/main.5540092c.chunk.js"
+  },
+  {
+    "revision": "75dceaf7d4aa0ee98f02",
+    "url": "./static/js/1.75dceaf7.chunk.js"
+  },
+  {
+    "revision": "5540092c179caa2267d7",
+    "url": "./static/css/main.b41bd053.chunk.css"
+  },
+  {
+    "revision": "dd720faf16ad113194dad801f8866252",
+    "url": "./index.html"
+  }
+];
